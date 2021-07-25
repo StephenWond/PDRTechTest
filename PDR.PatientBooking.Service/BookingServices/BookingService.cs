912153bd641d100d619db6c6ec412b9ca9509f15
StephenWond <stephen.wond@pickr.works>
2021-07-25 14:36:24 +0100

    ticket2 - Adding validation to the booking services

--- PDR.PatientBooking.Service/BookingServices/BookingService.cs
+++ PDR.PatientBooking.Service/BookingServices/BookingService.cs
@@ -1,25 +1,40 @@
+using System;
 using System.Linq;
 using Microsoft.Extensions.Internal;
 using PDR.PatientBooking.Data;
 using PDR.PatientBooking.Data.Models;
 using PDR.PatientBooking.Service.BookingServices.Requests;
 using PDR.PatientBooking.Service.BookingServices.Responses;
+using PDR.PatientBooking.Service.BookingServices.Validation;
 
 namespace PDR.PatientBooking.Service.BookingServices
 {
     public class BookingService : IBookingService
     {
         private readonly PatientBookingContext _context;
+        private readonly IBookingRequestValidator _validator;
         private readonly ISystemClock _systemClock;
 
-        public BookingService(PatientBookingContext context, ISystemClock systemClock)
+        public BookingService(
+            PatientBookingContext context,
+            ISystemClock systemClock,
+            IBookingRequestValidator validator
+        )
         {
             _context = context;
             _systemClock = systemClock;
+            _validator = validator;
         }
 
         public GetBookingResponse GetNextBooking(long patientId)
         {
+            var validationResult = _validator.ValidateRequest(patientId);
+
+            if (!validationResult.PassedValidation)
+            {
+                throw new ArgumentException(validationResult.Errors.First());
+            }
+
             var nextBooking =
                 _context.Order
                     .Where(x =>
@@ -46,16 +61,25 @@ namespace PDR.PatientBooking.Service.BookingServices
 
         public void AddBooking(AddBookingRequest request)
         {
-            _context.Order.Add(new Order
+            var validationResult = _validator.ValidateRequest(request);
+
+            if (!validationResult.PassedValidation)
             {
-                StartTime = request.StartTime,
-                EndTime = request.EndTime,
-                SurgeryType = (int) _context.Patient.FirstOrDefault(x => x.Id == request.PatientId).Clinic.SurgeryType,
-                PatientId = request.PatientId,
-                DoctorId = request.DoctorId,
-                Patient = _context.Patient.FirstOrDefault(x => x.Id == request.PatientId),
-                Doctor = _context.Doctor.FirstOrDefault(x => x.Id == request.DoctorId),
-            });
+                throw new ArgumentException(validationResult.Errors.First());
+            }
+
+            _context.Order.Add(new Order
+                {
+                    StartTime = request.StartTime,
+                    EndTime = request.EndTime,
+                    SurgeryType = (int) _context.Patient.FirstOrDefault(x => x.Id == request.PatientId).Clinic
+                        .SurgeryType,
+                    PatientId = request.PatientId,
+                    DoctorId = request.DoctorId,
+                    Patient = _context.Patient.FirstOrDefault(x => x.Id == request.PatientId),
+                    Doctor = _context.Doctor.FirstOrDefault(x => x.Id == request.DoctorId),
+                }
+            );
 
             _context.SaveChanges();
         }
---
+++ PDR.PatientBooking.Service/BookingServices/Validation/BookingRequestValidation.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Internal;
+using PDR.PatientBooking.Data;
+using PDR.PatientBooking.Service.BookingServices.Requests;
+using PDR.PatientBooking.Service.Validation;
+
+namespace PDR.PatientBooking.Service.BookingServices.Validation
+{
+    public class BookingRequestValidation : IBookingRequestValidator
+    {
+        private readonly PatientBookingContext _context;
+        private readonly ISystemClock _systemClock;
+
+        public BookingRequestValidation(PatientBookingContext context, ISystemClock systemClock)
+        {
+            _context = context;
+            _systemClock = systemClock;
+        }
+
+        public PdrValidationResult ValidateRequest(AddBookingRequest request)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (PatientNotFound(request.PatientId, ref result))
+                return result;
+
+            if (DoctorNotFound(request.DoctorId, ref result))
+                return result;
+
+            if (InvalidBookingDateTime(request, ref result))
+                return result;
+
+            if (DoctorNotAvailable(request, ref result))
+                return result;
+
+            return result;
+        }
+
+        public PdrValidationResult ValidateRequest(long patientId)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (PatientNotFound(patientId, ref result))
+                return result;
+
+            return result;
+        }
+
+        public PdrValidationResult ValidateRequest(Guid bookingId)
+        {
+            var result = new PdrValidationResult(true);
+
+            if (BookingNotFound(bookingId, ref result))
+                return result;
+
+            return result;
+        }
+
+        private bool PatientNotFound(long patientId, ref PdrValidationResult result)
+        {
+            if (!_context.Patient.Any(x => x.Id == patientId))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A patient with that ID could not be found");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DoctorNotFound(long doctorId, ref PdrValidationResult result)
+        {
+            if (!_context.Doctor.Any(x => x.Id == doctorId))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A doctor with that ID could not be found");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool InvalidBookingDateTime(AddBookingRequest request, ref PdrValidationResult result)
+        {
+            var errors = new List<string>();
+
+            if (request.StartTime < _systemClock.UtcNow.UtcDateTime)
+                errors.Add("An appointment cannot be booked in the past");
+
+            if (request.StartTime >= request.EndTime)
+                errors.Add("An appointment end time must be after the start time");
+
+            if (errors.Any())
+            {
+                result.PassedValidation = false;
+                result.Errors.AddRange(errors);
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DoctorNotAvailable(AddBookingRequest request, ref PdrValidationResult result)
+        {
+            var doctor = _context.Doctor.FirstOrDefault(x => x.Id == request.DoctorId);
+
+            if (doctor.Orders.Any(x => request.StartTime <= x.EndTime && request.EndTime >= x.StartTime))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("The requested appointment time with this doctor is not available");
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool BookingNotFound(Guid bookingId, ref PdrValidationResult result)
+        {
+            if (!_context.Order.Any(x => x.Id == bookingId))
+            {
+                result.PassedValidation = false;
+                result.Errors.Add("A booking with that ID could not be found");
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
